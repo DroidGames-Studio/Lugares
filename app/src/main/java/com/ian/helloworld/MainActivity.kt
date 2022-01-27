34bbc2ad8adc2eef54f52549cfda09fb42a8fa03
Ian Fallas <ian.f1999@gmail.com>
2022-01-26 20:53:41 -0600

    Initial commit

---
+++ app/src/main/java/com/ian/helloworld/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.ian.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+private lateinit var auth: FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+    }
+}
